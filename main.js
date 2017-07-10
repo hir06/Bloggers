c8945d9860d084485d851f122579aaff3efa240e
Mona Lisa <phiral246@gmail.com>
2017-07-10 22:51:07 +0530

    Corrected detail.jsx case

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import Home from './Home.jsx';
-import Detail from './Detail.jsx';
+import Detail from './detail.jsx';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render((
-  <HashRouter>
-    <App>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/detail/:name' component={Detail} />
-      </Switch>
-    </App>
-  </HashRouter>
-), document.getElementById('app'))
+
+    ReactDOM.render((
+      <HashRouter>
+        <App>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/detail/:name' component={Detail} />
+          </Switch>
+        </App>
+      </HashRouter>
+    ), document.getElementById('app'))
